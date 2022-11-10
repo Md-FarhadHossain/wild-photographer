83be727315da01d3b1d06006a88b1988355ea9e3
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-11-10 22:28:43 +0600

    blog page responsive done

--- src/pages/Blog/BlogContent.jsx
+++ src/pages/Blog/BlogContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogContent = ({title, description}) => {
   return (
-    <div className="container mx-auto my-8">
+    <div className="container mx-auto px-4 my-8">
       <div className="mockup-window border bg-base-300">
         <div className="flex flex-col px-4 p-8 bg-base-200">
             <h1 className="text-3xl font-bold">{title}</h1>
